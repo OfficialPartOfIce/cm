6f7ac193c35acc025f7191d2a2d22f127d31f91c
partofice <officialpartofice@gmail.com>
2019-02-08 12:47:13 +0530

    Updates

---
+++ precache-manifest.3d4493469a7f7adf124078f6137b3173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f24e863ff8fd6e21769",
+    "url": "/cm/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "0f24e863ff8fd6e21769",
+    "url": "/cm/static/js/main.0f24e863.chunk.js"
+  },
+  {
+    "revision": "9b0994eb202e73ad133c",
+    "url": "/cm/static/css/1.855bd37a.chunk.css"
+  },
+  {
+    "revision": "9b0994eb202e73ad133c",
+    "url": "/cm/static/js/1.9b0994eb.chunk.js"
+  },
+  {
+    "revision": "c003474b2c7d302839ed",
+    "url": "/cm/static/js/runtime~main.c003474b.js"
+  },
+  {
+    "revision": "1ecd5c6b24ce53397ba49ad6deb93528",
+    "url": "/cm/index.html"
+  }
+];
